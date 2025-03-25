379f2daed5d90ef2e49ced57d234a836bb1be14e
Show Wai Yan <showwaiyan555@gmail.com>
2025-03-25 09:49:15 +0800

    Connect to DB

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const dotenv = require("dotenv");
 const app = require("./app");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 
-console.log(process.env);
+// console.log(process.env);
+
+const DB = process.env.DB_REMOTE.replace("<DB_PASSWORD>", process.env.DB_PASSWORD);
+mongoose
+	// .connect(DB)
+	.connect(process.env.DB_LOCAL)
+	.then((conn) => {
+		console.log("DB is connected");
+	});
 
 const port = process.env.PORT || 5500;
 app.listen(port, () => {
