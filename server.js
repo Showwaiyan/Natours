6930a6c5667b11b365f4d76f9a22f059e100a702
Show Wai Yan <showwaiyan555@gmail.com>
2025-03-25 10:21:53 +0800

    Create Tour's schema and model

--- server.js
+++ server.js
@@ -7,12 +7,35 @@ dotenv.config({ path: "./config.env" });
 // console.log(process.env);
 
 const DB = process.env.DB_REMOTE.replace("<DB_PASSWORD>", process.env.DB_PASSWORD);
+const DB_OPTION = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	retryWrites: true,
+};
 mongoose
-	// .connect(DB)
+	// .connect(DB,DB_OPTION)
 	.connect(process.env.DB_LOCAL)
 	.then((conn) => {
 		console.log("DB is connected");
 	});
+
+const tourSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		require: [true, "Tour must have name!"],
+	},
+	rating: {
+		type: Number,
+		default: 2.5,
+	},
+	price: {
+		type: Number,
+		require: [true, "Each tour is need pricing"],
+	},
+});
+
+const Tour = mongoose.model("Model", tourSchema);
 
 const port = process.env.PORT || 5500;
 app.listen(port, () => {
