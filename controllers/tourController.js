0c6afd1dba8b4bb701b9535a29896d321d432c8c
Show Wai Yan <showwaiyan555@gmail.com>
2025-03-26 10:25:29 +0800

    Implement getting all tours and a tour by ID

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,24 +13,40 @@ const Tour = require("./../models/tourModel");
 // 	next();
 // };
 
-exports.getAllTours = (req, res) => {
-	res.status(200).json({
-		status: "success",
-		requestedAt: req.requestTime,
-		// results: tours.length,
-		// data: {
-		// 	tours,
-		// },
-	});
+exports.getAllTours = async (req, res) => {
+	try {
+		const tours = await Tour.find();
+		res.status(200).json({
+			status: "success",
+			requestedAt: req.requestTime,
+			results: tours.length,
+			data: {
+				tours,
+			},
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: "fail",
+			message: { error },
+		});
+	}
 };
 
-exports.getTour = (req, res) => {
-	res.status(200).json({
-		status: "success",
-		// data: {
-		// 	tour,
-		// },
-	});
+exports.getTour = async (req, res) => {
+	try {
+		const tour = await Tour.findById(req.params.id);
+		res.status(200).json({
+			status: "success",
+			data: {
+				tour,
+			},
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: "fail",
+			message: { error },
+		});
+	}
 };
 
 exports.createTour = async (req, res) => {
