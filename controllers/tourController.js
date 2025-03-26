19ff93175b87a8a6eb59c49eb5d3809c068602ed
Show Wai Yan <showwaiyan555@gmail.com>
2025-03-26 16:36:40 +0800

    Implement delete tour functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -88,9 +88,14 @@ exports.updateTour = async (req, res) => {
 	}
 };
 
-exports.deleteTour = (req, res) => {
-	res.status(204).json({
-		status: "success",
-		data: null,
-	});
+exports.deleteTour = async (req, res) => {
+	try {
+		await Tour.findByIdAndDelete(req.params.id);
+		res.status(204);
+	} catch (error) {
+		res.status(400).json({
+			status: "fail",
+			message: { error },
+		});
+	}
 };
