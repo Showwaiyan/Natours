43dde918c37ab2c5f8bdb90fac2e979b142d12b3
Show Wai Yan <showwaiyan555@gmail.com>
2025-04-07 12:51:51 +0800

    Change DB schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,19 +1,54 @@
 const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema({
-	title: {
+	name: {
 		type: String,
 		required: [true, "Tour must have name!"],
 		unique: true,
 	},
-	rating: {
+	duration: {
 		type: Number,
-		default: 2.5,
+		required: [true, "Tour must have Duration!"],
+	},
+	maxGroupSize: {
+		type: Number,
+		required: [true, "Tour must have Maximum Group Size!"],
+	},
+	difficulty: {
+		type: String,
+		required: [true, "Tour must have difficulty"],
+	},
+	ratingAverage: {
+		type: Number,
+		default: 0,
+	},
+	ratingQuantity: {
+		type: Number,
+		default: 0,
 	},
 	price: {
 		type: Number,
 		required: [true, "Each tour is need pricing"],
 	},
+	priceDiscount: Number,
+	summary: {
+		type: String,
+		required: [true, "Tour must have summary!"],
+	},
+	description: {
+		type: String,
+		required: [true, "Tour must have description!"],
+	},
+	imageCover: {
+		type: String,
+		required: [true, "Tour must have Image Cover"],
+	},
+	images: [String],
+	createAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	startDates: [Date],
 });
 
 const Tour = mongoose.model("Model", tourSchema);
