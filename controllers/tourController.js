3954e04a15fc4c3682001ef9804d743f76746ccd
Show Wai Yan <showwaiyan555@gmail.com>
2025-04-14 12:37:48 +0800

    Implement MongoDB matching and grouping pipline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -109,3 +109,39 @@ exports.deleteTour = async (req, res) => {
 		});
 	}
 };
+
+exports.getToursStats = async (req, res) => {
+	try {
+		const stats = await Tour.aggregate([
+			{
+				$match: { ratingAverage: { $lte: 4.5 } },
+			},
+			{
+				$group: {
+					_id: { $toUpper: "$difficulty" },
+					numTours: { $sum: 1 },
+					numRating: { $sum: "$ratingQuantity" },
+					avgRating: { $avg: "$ratingAverage" },
+					avgPrice: { $avg: "$price" },
+					minPrice: { $min: "$price" },
+					maxPrice: { $max: "$price" },
+				},
+			},
+			{
+				$sort: { avgPrice: 1 },
+			},
+		]);
+
+		res.status(200).json({
+			status: "success",
+			data: {
+				stats,
+			},
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: "fetch stats fail",
+			message: { error },
+		});
+	}
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 // router.param("id", tourController.checkID);
 router.route("/top-5-cheap").get(tourController.getTopFiveCheapTours, tourController.getAllTours);
+router.route("/tours-stats").get(tourController.getToursStats);
 router.route("/").get(tourController.getAllTours).post(tourController.createTour);
 
 router.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
