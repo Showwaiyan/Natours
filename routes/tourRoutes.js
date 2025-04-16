470218101fd607ef84f91a2f5e1007c69e95ace4
Show Wai Yan <showwaiyan555@gmail.com>
2025-04-16 11:09:07 +0800

    Implement Monthly Tours summarize route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 // router.param("id", tourController.checkID);
 router.route("/top-5-cheap").get(tourController.getTopFiveCheapTours, tourController.getAllTours);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router.route("/tours-stats").get(tourController.getToursStats);
+
 router.route("/").get(tourController.getAllTours).post(tourController.createTour);
 
 router.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
