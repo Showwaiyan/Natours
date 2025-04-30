693aec7b2cb0b7a7b6d1a49192dc435c23b1a705
Show Wai Yan <showwaiyan555@gmail.com>
2025-04-30 11:40:19 +0800

    Create custom error class and error controller

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utilities/appError");
+
+const globalErrorHandler = require("./controllers/errorController");
+
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -25,22 +29,14 @@ app.use("/api/v1/users", userRouter);
 
 // Handle Unhandle Route
 app.all("*", (req, res, next) => {
-	const err = new Error(`Can't find ${req.originalUrl} on the server!`);
-	err.statusCode = 404;
-	err.status = "Fail";
+	// const err = new Error(`Can't find ${req.originalUrl} on the server!`);
+	// err.statusCode = 404;
+	// err.status = "Fail";
 
-	next(err);
+	next(new AppError(`Can't find ${req.originalUrl} on the server!`, 404));
 });
 
 // Express Error Handle Middle
-app.use((err, req, res, next) => {
-	err.statusCode = err.statusCode || 500;
-	err.status = err.status || "Erro";
-
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message,
-	});
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
