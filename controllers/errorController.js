1ab5cd9df783c2e0a7511c1ac157a41b45ff23b5
Show Wai Yan <showwaiyan555@gmail.com>
2025-05-01 20:52:34 +0800

    Implement error respond between development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,34 @@
+const sendErrorDev = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		error: err,
+		message: err.message,
+		stack: err.stack,
+	});
+};
+
+const sendErroProd = (err, res) => {
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message,
+		});
+	} else {
+		console.error(`Error💥: ${err}`);
+		res.status(500).json({
+			status: err.status,
+			message: "Something went wrong!",
+		});
+	}
+};
+
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || "Error";
 
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message,
-	});
+	if (process.env.NODE_ENV === "development") {
+		sendErrorDev(err, res);
+	} else if (process.env.NODE_ENV === "production") {
+		sendErroProd(err, res);
+	}
 };
