a63fdecf791bafe1388fbd5e0262e28fc3e43b6c
Show Wai Yan <showwaiyan555@gmail.com>
2025-05-01 21:08:32 +0800

    Implement DB's invalid ID custome error Message return

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require("./../utilities/appError");
+
+const handleCastErrorDB = (err) => {
+	const message = `Invalid ${err.path}: ${err.value}`;
+	return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
 	res.status(err.statusCode).json({
 		status: err.status,
@@ -29,6 +35,9 @@ module.exports = (err, req, res, next) => {
 	if (process.env.NODE_ENV === "development") {
 		sendErrorDev(err, res);
 	} else if (process.env.NODE_ENV === "production") {
-		sendErroProd(err, res);
+		let error = { ...err };
+
+		if (error.name === "CastError") error = handleCastErrorDB(error);
+		sendErroProd(error, res);
 	}
 };
