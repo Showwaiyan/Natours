26c4cfdbf1c1ab4f13cbffbb5dc9c20b14269e2d
Show Wai Yan <showwaiyan555@gmail.com>
2025-07-12 22:11:08 +0800

    Clean xss

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
 
 const AppError = require("./utilities/appError");
 
@@ -32,6 +33,7 @@ app.use(express.json({limit: "10kb"}));
 app.use(express.static(`${__dirname}/public`));
 
 app.use(mongoSanitize());
+app.use(xss())
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
