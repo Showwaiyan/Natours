67d349ea0d2fddab6b3ffea3ebc2f007e4748c37
Show Wai Yan <showwaiyan555@gmail.com>
2025-07-14 09:42:16 +0800

    Add child referencing from tours to users

--- models/tourModel.js
+++ models/tourModel.js
@@ -44,7 +44,7 @@ const tourSchema = new mongoose.Schema(
     priceDiscount: {
       type: Number,
       validate: {
-        validator: function(value) {
+        validator: function (value) {
           return value < this.price;
         },
         message: "Discount price {VALUE} must be lower than regular price1",
@@ -89,7 +89,13 @@ const tourSchema = new mongoose.Schema(
         coordinates: [Number],
         address: String,
         description: String,
-        day: Number
+        day: Number,
+      },
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
       },
     ],
   },
@@ -98,11 +104,11 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   },
 );
-tourSchema.virtual("durationWeek").get(function() {
+tourSchema.virtual("durationWeek").get(function () {
   return this.duration / 7;
 });
 
-tourSchema.pre("save", function() {
+tourSchema.pre("save", function () {
   this.slugify = slugify(this.name, { lower: true });
 });
 
