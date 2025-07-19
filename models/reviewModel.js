780547789315c9a88f00a72088426dd771ce0fd5
Show Wai Yan <showwaiyan555@gmail.com>
2025-07-19 11:11:49 +0800

    Implment rating average and rating quantity computing for tour when review is updated and deleted

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,7 +35,7 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
-reviewSchema.pre(/^find/, function(next) {
+reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: "user",
     select: "name photo",
@@ -43,7 +43,7 @@ reviewSchema.pre(/^find/, function(next) {
   next();
 });
 
-reviewSchema.statics.calcAverageAndTotal = async function(tourId) {
+reviewSchema.statics.calcAverageAndTotal = async function (tourId) {
   const stats = await this.aggregate([
     {
       $match: { tour: tourId },
@@ -56,18 +56,24 @@ reviewSchema.statics.calcAverageAndTotal = async function(tourId) {
       },
     },
   ]);
-  return stats;
-};
-
-reviewSchema.post("save", async function() {
-  const stats = await this.constructor.calcAverageAndTotal(this.tour);
-
-  const filter = { _id: this.tour };
+  const filter = { _id: tourId };
   const update = {
-    ratingQuantity: stats[0].ratingQuantity,
-    ratingAverage: stats[0].ratingAverage,
+    ratingQuantity: stats.length ? stats[0].ratingQuantity : 0,
+    ratingAverage: stats.length ? stats[0].ratingAverage : 1,
   };
   await Tour.findOneAndUpdate(filter, update);
+};
+
+reviewSchema.post("save", async function () {
+  await this.constructor.calcAverageAndTotal(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.clone().findOne();
+  next();
+});
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverageAndTotal(this.r.tour);
 });
 
 const Review = mongoose.model("Reviews", reviewSchema);
