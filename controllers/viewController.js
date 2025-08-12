7d0be3037eb15a84600f314ec424d72e5d99bdf9
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-12 15:07:59 +0800

    Render tour cards in overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,14 +1,19 @@
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utilities/catchAsync");
+
 exports.getHome = (req, res) => {
   res.status(200).render("base", {
     title: "Welcome",
   });
 };
 
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
   res.status(200).render("overview", {
     title: "All Tours",
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render("tour", {
