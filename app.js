fac63b773e641fce43c5719a8e0fc11507858a72
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-15 12:07:14 +0800

    Add Cookie praser for jwt auth

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utilities/appError");
 
@@ -58,6 +59,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
 
 app.use(mongoSanitize());
 app.use(xss());
@@ -77,6 +79,7 @@ app.use(
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
