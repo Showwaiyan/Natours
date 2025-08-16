9b1804af07932257e04f49fd4c9d77974135c67e
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-16 11:36:22 +0800

    Render alert component into body

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {showAlert} from "./alert"
 
 export function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -9,18 +10,18 @@ export function validateEmail(email) {
 export async function login(email, password) {
   try {
     const url = "http://localhost:5500/api/v1/users/login";
-    const respond = await axios.post(url, {
+    const response = await axios.post(url, {
       email,
       password,
     });
-    const result = await respond.data;
-    if (respond.data.status === "success") {
-      alert("Logged In Successfully");
+    const result = await response.data;
+    if (result.status === "success") {
+      showAlert("success","Logged In Successfully")
       window.setTimeout(() => {
         window.location.assign("/");
       }, 1500);
     }
   } catch (error) {
-    alert(error.respond.data.message);
+    showAlert("error",error.response.data.message);
   }
 }
