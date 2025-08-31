3263718065e382f950144f5b6b5ad5e3c0d86488
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-31 09:49:27 +0800

    Add logs for error to be shown in deployment

--- server.js
+++ server.js
@@ -4,39 +4,42 @@ dotenv.config({ path: "./config.env" });
 const app = require("./app");
 const mongoose = require("mongoose");
 
-
 // console.log(process.env);
 process.on("unhandledRejection", (err) => {
-	console.log(err.name);
-	console.log(err.message);
+  console.log(err.name);
+  console.log(err.message);
 
-	server.close(() => {
-		process.exit(1);
-	});
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
 process.on("uncaughtException", (err) => {
-	console.log(err.name);
-	console.log(err.message);
+  console.log(err.name);
+  console.log(err.message);
 
-	process.exit(1);
+  process.exit(1);
 });
 
-const DB = process.env.DB_REMOTE.replace("<DB_PASSWORD>", process.env.DB_PASSWORD);
+const DB = process.env.DB_REMOTE.replace(
+  "<DB_PASSWORD>",
+  process.env.DB_PASSWORD,
+);
 const DB_OPTION = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-	retryWrites: true,
+  retryWrites: true,
 };
 mongoose
-	// .connect(DB,DB_OPTION)
-	.connect(process.env.DB_LOCAL)
-	.then((conn) => {
-		console.log("DB is connected");
-	});
+  .connect(
+    process.env.NODE_ENV === "production" ? DB : null,
+    process.env.NODE_ENV === "production" ? DB_OPTION : process.env.DB_LOCAL,
+  )
+  .then((conn) => {
+    console.log("DB is connected");
+  }).catch(err=>{
+    console.log(err)
+  });
 
 const port = process.env.PORT || 5500;
 const server = app.listen(port, () => {
-	console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
